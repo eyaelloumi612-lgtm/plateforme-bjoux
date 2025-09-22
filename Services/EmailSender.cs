d3020dda36f819a2d6b8ddcb4415ccd9d2f5d641
eyaelloumi612-lgtm <eyaelloumi612@gmail.com>
2025-09-22 23:00:39 +0100

    Add files via upload

---
+++ Services/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace JewelryManagementPlatform.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Implémentation factice - à remplacer par un vrai service d'email
+            return Task.CompletedTask;
+        }
+    }
+}
